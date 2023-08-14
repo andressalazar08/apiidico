f537066cc9676a9f944127767d468dc7e25bcfe5
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-14 16:35:57 -0500

    initial configuration and installations

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+
+var corOptions = {
+    origin: 'http://localhost:8081'
+}
+
+//middlewares
+app.use(cors(corOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/', (req,res)=>{
+    res.json({
+        message:'API for IDICO connected - Andres Salazar'
+    })
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: ${PORT}`)
+})
