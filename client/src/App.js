c52dfc6c7b3574622382d116aeb0c0aa02a7955a
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-15 09:18:17 -0500

    home component done

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router,  Route} from 'react-router-dom';
 import AddChar  from './views/AddChar';
 import ShowChar from './views/ShowChar';
 import EditChar from './views/EditChar';
-import DetailChar from './views/DetailChar'
+import DetailChar from './views/DetailChar';
+import Home from './views/Home';
+
 
 function App() {
   return (
     <Router>
-
+          <Route exact path='/' component={Home}/>
           <Route exact path='/addChar' component={AddChar}/>
           <Route exact path='/getChars' component={ShowChar}/>
           <Route exact path='/char/edit/:id' component={EditChar}/>
---
+++ client/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+  return (
+    <div className='App'>
+        <h1>Home Idico APi</h1>
+        <h4>Andrés Felipe Salazar Ramos</h4>
+    </div>
+  )
+}
+
+export default Home
