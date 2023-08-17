541e6024702a5d4fa731fb4f9e64d2aad4021f98
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-17 12:46:42 -0500

    get movies by query name genre and order by creationdate

--- api/controllers/charController.js
+++ api/controllers/charController.js
@@ -143,7 +143,7 @@ const queryCharacters =  async(req,res)=>{
     }
 
 }
-
+//7. search a movie by its id
 const moviesInfo= async(req,res)=>{
 
     let id = req.params.id
@@ -159,7 +159,7 @@ const moviesInfo= async(req,res)=>{
     }
 }
 
-
+//8. find all movies in Db
 const allMovies = async(req,res)=>{
 
     const movies = await Mos.findAll({})
@@ -173,6 +173,48 @@ const allMovies = async(req,res)=>{
         }
 }
 
+//9. Find movies by querys
+const queryMovie = async(req,res)=>{
+
+    let title = req.query.title
+    let stars = req.query.stars
+    let order = req.query.order
+
+    if(title){
+        const movie = await Mos.findOne({where:{title:title}})
+        if(movie){
+            res.status(200).send(movie)
+        }else{
+            res.status(404).json({
+                message:"Movie not found"
+            })
+        }
+    }
+
+    if(stars){
+        const movies = await Mos.findAll({where:{starts:stars}})
+        if(movies.length>0){
+            res.status(200).send(movies)
+        }else{
+            res.status(404).json({
+                message:"Not movies found with those stars"
+            })
+        }
+    }
+
+    if(order){
+        const movies = await Mos.findAll({order:[['creationdate',order]]})
+        if(movies.length>0){
+            res.status(200).send(movies)
+        }else{
+            res.status(404).json({
+                message: "There is not movies to order by"
+            })
+        }
+    }
+
+}
+
 module.exports = {
     addChar,
     getAllChar,
@@ -181,6 +223,7 @@ module.exports = {
     deleteChar,
     queryCharacters,
     moviesInfo,
-    allMovies
+    allMovies,
+    queryMovie
 
 }
--- api/routes/charRouter.js
+++ api/routes/charRouter.js
@@ -8,12 +8,15 @@ const router = require('express').Router();
 router.post('/addChar', charController.addChar)
 router.get('/allCharacters', charController.getAllChar)
 //route to find characters by query
-router.get('/find', charController.queryCharacters)
+router.get('/findCharacter', charController.queryCharacters)
 //route to get all movies info by id
 router.get('/allMovies', charController.allMovies)
+//route to find movies by query
+router.get('/findMovie', charController.queryMovie)
+
+//routes with ids
 //route to get a movie by id
 router.get('/movie/:id', charController.moviesInfo)
-
 router.get('/character/:id', charController.getOneChar)
 router.put('/character/:id', charController.updateChar)
 router.delete('/character/:id', charController.deleteChar)
